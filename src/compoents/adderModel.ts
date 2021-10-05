775c7e88ec6a25aab5bf7d4de528c6ab25161d82
Ashwin <naikashwin30@gmail.com>
2021-10-05 23:58:39 +0530

    added adderModel

---
+++ src/compoents/adderModel.ts
@@ -0,0 +1,14 @@
+export default interface adder {
+    ans: number,
+    showAns: boolean,
+    no1: number,
+    no2: number,
+    no1Range: {
+        max: number,
+        min: number
+    },
+    no2Range: {
+        max: number,
+        min: number
+    }
+}
